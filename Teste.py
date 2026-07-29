89bb3339469b7dd3af4bcbb184b8c6b77911e176
unknown <arthurndusco@gmail.com>
2026-07-29 13:15:03 -0300

    extensoes teste

--- Teste.py
+++ Teste.py
@@ -1,10 +1,10 @@
 print("Ola mundo")
 print("Este é um teste de código em Python.")
 while True:
-        a = input("digite 1: ")
-        if a == "1":
-            print("Você digitou 1!")
-            break
-        else:
-            print("Você não digitou 1.")
-print("Fim do teste.")
+    print("Digite 'sair' para encerrar o programa.")
+    user_input = input("Digite algo: ")
+    if user_input.lower() == "sair":
+        print("Encerrando o programa...")
+        break
+    else:
+        print(f"Você digitou: {user_input}")
