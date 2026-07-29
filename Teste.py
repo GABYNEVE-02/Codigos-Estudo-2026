7b64f0085391ac4133746cc2eca0e0fb79c596c8
unknown <arthurndusco@gmail.com>
2026-07-29 13:38:08 -0300

    desfiz os coiso

--- Teste.py
+++ Teste.py
@@ -1,10 +1,2 @@
 print("Ola mundo")
 print("Este é um teste de código em Python.")
-while True:
-    print("Digite 'sair' para encerrar o programa.")
-    user_input = input("Digite algo: ")
-    if user_input.lower() == "sair":
-        print("Encerrando o programa...")
-        break
-    else:
-        print(f"Você digitou: {user_input}")
