14d698fe7b32815eaf46d61fdc6c63041266b9cf
unknown <arthurndusco@gmail.com>
2026-07-29 12:23:14 -0300

    teste extensoes

--- Teste.py
+++ Teste.py
@@ -1,2 +1,9 @@
 print("Ola mundo")
 print("Este é um teste de código em Python.")
+while True:
+        a = input("digite 1: ")
+        if a == "1":
+            print("Você digitou 1!")
+            break
+        else:
+            print("Você não digitou 1.")
